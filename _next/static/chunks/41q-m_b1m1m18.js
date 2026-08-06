8f5d7193abfd1bd0006d78e7a219551dd8d94c03
abdulboriyx <abdulboriykhusanov19@gmail.com>
2026-08-06 17:20:14 +0900

    Publish static export for GitHub Pages root

---
+++ _next/static/chunks/41q-m_b1m1m18.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,37381,t=>{t.v(e=>Promise.all(["static/chunks/00ejb5-l_i4ia.js"].map(e=>t.l(e))).then(()=>e(64595)))}]);
